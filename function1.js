8c34dc7f68d0150478d82a284b250e689188bee2
Thrivenimuthyala <thrivenimuthyala03@gmail.com>
2025-04-05 06:45:13 +0530

    Add files via upload

---
+++ function1.js
@@ -0,0 +1,32 @@
+// // function myFunction(p1, p2) {
+// //     return p1 * p2;
+// //   }
+  
+// //   let result = myFunction(4, 3);
+// myfunction();
+// anotherFunc();
+// let petName;
+// function myfunction() {
+//     let petName = "Sizzer"; // local variable
+//     console.log(petName);
+// }
+// function anotherFunc() {
+//     let petName = "Tom"; // local variable
+//     console.log(petName);
+// }
+// console.log(petName);
+function sum(x,y){
+    return x+y;
+}
+console.log(sum(3,4));
+function apple(name){
+    return ("hello "+name+" !");
+}
+let Name="thriveni"
+console.log(apple(Name));
+const mul = function (x, y) {
+    return x * y;
+};
+console.log(mul(4, 5)); 
+
+
---
+++ operate.js
@@ -0,0 +1,8 @@
+var x=2;
+var y=3;
+var z=x*y;
+var a=x+y;
+var b=x-y;
+var c=x/y;
+var d=x|y;
+console.log(z,a,b,c,d);
