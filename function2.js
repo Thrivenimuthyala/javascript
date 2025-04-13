2fae0225d3eb77c6babc78005230019e6cc22d4b
Thrivenimuthyala <thrivenimuthyala03@gmail.com>
2025-04-13 23:03:39 +0530

    Add files via upload

---
+++ function2.js
@@ -0,0 +1,16 @@
+//1st
+let great=(user)=>{
+    console.log("hello thriveni "+user);
+    return 1 ;
+}
+console.log(great("thirri"));
+//2nd
+let apple=(obj)=>{
+    return "hi!"
+}
+console.log(apple()+" how are you");
+//3rd
+const sum=(num1,num2)=>num1+num2;
+console.log(sum(6,4));
+console.log("additiion: "+sum(6,4));
+    console.log(great("how")+apple());
---
+++ loop1.js
@@ -0,0 +1,28 @@
+// console.log("hello");
+// let i = 1;
+// while(i<=5)
+// {
+//     console.log(" hi thirri");
+//     i++;
+// }
+// //2nd
+// let j = 5;
+// while(j>=0){
+// console.log("how are you");
+// j--;
+// }
+//3rd
+// let i=5;
+// for(i=1;i<=3;i++)
+// {
+//     console.log("hi");
+//     for(j=1;j<=2;j++){
+//         console.log("thirri");
+//     }
+// }
+//4th
+let i=7;
+do{
+    console.log("thriveni");
+    i++;
+}while(i<=4)
