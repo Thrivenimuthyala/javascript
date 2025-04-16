520f2abf66013cb854878abd28fb187790a26664
Thrivenimuthyala <thrivenimuthyala03@gmail.com>
2025-04-16 09:40:19 +0530

    Add files via upload
    array method

---
+++ array.js
@@ -0,0 +1,10 @@
+// // let a=[1,2,3,4]
+// // // console.log(a);
+// let fruit=["apple","banana","orange"];
+// console.log(fruit.length);
+let place=['knr','hyd','jgl'];
+// let length=place.push('mll')
+// let length=place.pop()
+// let length=place.shift()
+let length=place.unshift('56')
+console.log(place);
